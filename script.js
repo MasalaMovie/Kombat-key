40ff608356d433be0adef363c93b282d45c57690
MasalaMovie <168056962+MasalaMovie@users.noreply.github.com>
2024-08-08 18:17:55 +0545

    Update script.js

--- script.js
+++ script.js
@@ -262,10 +262,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     document.getElementById('creatorChannelBtn').addEventListener('click', () => {
-        window.open('https://telegram.me/Insta_Buy_Follower', '_blank');
+        window.open('https://t.me/HamsterKombatskeys', '_blank');
     });
 
     telegramChannelBtn.addEventListener('click', () => {
-        window.open('https://telegram.me/Sam_Dm_bot', '_blank');
+        window.open('https://telegram.me/MasalaUniverseContact_robot', '_blank');
     });
 });
