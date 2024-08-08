9636a3cba5933739fbbb12d0c05ef3a8c67a75e8
MasalaMovie <168056962+MasalaMovie@users.noreply.github.com>
2024-08-08 18:13:16 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const EVENTS_DELAY = 20000;
-    const MAX_KEYS_PER_GAME_PER_DAY = 100000;
+    const MAX_KEYS_PER_GAME_PER_DAY = 10000;
 
     const games = {
         1: {
